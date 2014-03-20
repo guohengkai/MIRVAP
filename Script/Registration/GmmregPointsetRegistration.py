e1f361a99c9c3fc1ebecf5f040c360370af33bfb
guohengkai <guohengkaighk@gmail.com>
2014-03-20 16:01:49 +0800

    Apply transform to centerline

--- Script/Registration/GmmregPointsetRegistration.py
+++ Script/Registration/GmmregPointsetRegistration.py
@@ -43,7 +43,7 @@ class GmmregPointsetRegistration(RegistrationBase):
         fixed[:, :3] *= fixed_res[:3]
         moving[:, :3] *= moving_res[:3]
         if (fixed_bif >= 0) and (moving_bif >= 0):
-            fixed[:, 2] -= (fixed_bif * fixed_res[2] - moving_bif * moving_res[2]);
+            fixed[:, 2] -= (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
         
         eg.initial_data(fixed, moving)
         code = eg.run_executable()
@@ -53,7 +53,7 @@ class GmmregPointsetRegistration(RegistrationBase):
         
         trans_points = trans;
         if (fixed_bif >= 0) and (moving_bif >= 0):
-            trans_points[:, 2] += (fixed_bif * fixed_res[2] - moving_bif * moving_res[2]);
+            trans_points[:, 2] += (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
         trans_points[:, :3] /= fixed_res[:3]
         trans_points = npy.insert(trans, [trans.shape[1]], moving_points[:, -1].reshape(-1, 1), axis = 1)
         trans_points = npy.append(trans_points, npy.array([[-1, -1, -1, -1]]), axis = 0)
@@ -66,11 +66,25 @@ class GmmregPointsetRegistration(RegistrationBase):
         
         C2 = npy.asmatrix(para2[4:7]).T
         T0 = npy.asmatrix(para[4:]).T
-        T = S1 * T0 + C2 - C
-        T = -T
         R = self.quaternion2rotation(para[:4])
+        T = S1 * T0 + C2 - C
         if (fixed_bif >= 0) and (moving_bif >= 0):
-            T[2] -= (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
+            T[2] += (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
+            
+        moving_center = movingData.getPointSet('Centerline').copy();
+        result_center = moving_center[npy.where(moving_center[:, 0] >= 0)]
+        result_center[:, :3] *= moving_res[:3]
+        result_center[:, :3] -= C.T
+        temp = ml.mat(result_center[:, :3]) * R + ml.ones((result_center.shape[0], 1)) * (T + C).T
+        
+        result_center[:, :3] = temp
+        result_center[:, :3] /= fixed_res[:3]
+        result_center = npy.append(result_center, npy.array([[-1, -1, -1, -1]]), axis = 0)
+        print result_center
+        print fixedData.getPointSet('Centerline')
+        
+        
+        T = -T
         T = R * T
         
         transform = sitk.Transform(3, sitk.sitkAffine)
@@ -81,7 +95,7 @@ class GmmregPointsetRegistration(RegistrationBase):
         fixedImage = fixedData.getSimpleITKImage()
         resultImage = sitk.Resample(movingImage, fixedImage, transform, sitk.sitkLinear, 0, sitk.sitkFloat32)
         
-        return sitk.GetArrayFromImage(resultImage), {'Contour': trans_points}
+        return sitk.GetArrayFromImage(resultImage), {'Contour': trans_points, 'Centerline': result_center}
     def quaternion2rotation(self, q):
         R = ml.zeros([3, 3], dtype = npy.float32)
         x, y, z, r = q
