06f7eab6b4727fe79c529c1a87bcfffdfb8842f8
guohengkai <guohengkaighk@gmail.com>
2014-03-24 16:29:52 +0800

    Add vtk-based icp registration

--- Script/Registration/vtkIcpPointsetRegistration.py
+++ Script/Registration/vtkIcpPointsetRegistration.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on 2014-03-16
+Created on 2014-03-24
 
 @author: Hengkai Guo
 """
 
 from MIRVAP.Script.RegistrationBase import RegistrationBase
 import MIRVAP.Core.DataBase as db
-import MIRVAP.ThirdParty.gmmreg.executeGmmreg as eg
 import numpy as npy
 import numpy.matlib as ml
-import itk
+import itk, vtk
 import SimpleITK as sitk
 
-class GmmregPointsetRegistration(RegistrationBase):
+class vtkIcpPointsetRegistration(RegistrationBase):
     def __init__(self, gui):
-        super(GmmregPointsetRegistration, self).__init__(gui)
+        super(vtkIcpPointsetRegistration, self).__init__(gui)
     def getName(self):
-        return 'GMMREG Pointset Registration For Vessel'
+        return 'ICP Pointset Registration For Vessel'
                                  
     def register(self, fixedData, movingData):
         fixed_points = fixedData.getPointSet('Contour')
@@ -45,37 +44,64 @@ class GmmregPointsetRegistration(RegistrationBase):
         if (fixed_bif >= 0) and (moving_bif >= 0):
             fixed[:, 2] -= (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
         
-        eg.initial_data(fixed, moving)
-        code = eg.run_executable()
-        if code != 0:
-            return None, None
-        trans, para, para2 = eg.get_final_result()
+        sourcePoints = vtk.vtkPoints()
+        sourceVertices = vtk.vtkCellArray()
+        for x in moving:
+            id = sourcePoints.InsertNextPoint(x[0], x[1], x[2])
+            sourceVertices.InsertNextCell(1)
+            sourceVertices.InsertCellPoint(id)
+        source = vtk.vtkPolyData()
+        source.SetPoints(sourcePoints)
+        source.SetVerts(sourceVertices)
         
-        trans_points = trans;
+        targetPoints = vtk.vtkPoints()
+        targetVertices = vtk.vtkCellArray()
+        for x in fixed:
+            id = targetPoints.InsertNextPoint(x[0], x[1], x[2])
+            targetVertices.InsertNextCell(1)
+            targetVertices.InsertCellPoint(id)
+        target = vtk.vtkPolyData()
+        target.SetPoints(targetPoints)
+        target.SetVerts(targetVertices)
+        
+        icp = vtk.vtkIterativeClosestPointTransform()
+        icp.SetSource(source)
+        icp.SetTarget(target)
+        icp.GetLandmarkTransform().SetModeToRigidBody()
+        icp.Modified()
+        icp.Update()
+        
+        icp_filter = vtk.vtkTransformPolyDataFilter()
+        icp_filter.SetInput(source)
+        icp_filter.SetTransform(icp)
+        icp_filter.Update()
+        result_source = icp_filter.GetOutput()
+        
+        n = result_source.GetNumberOfPoints()
+        trans_points = npy.empty([n, 3], dtype = npy.float32)
+        for i in range(n):
+            temp = [0.0, 0.0, 0.0]
+            result_source.GetPoint(i, temp)
+            trans_points[i, :] = npy.array(temp)
+        
         if (fixed_bif >= 0) and (moving_bif >= 0):
             trans_points[:, 2] += (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
         trans_points[:, :3] /= fixed_res[:3]
-        trans_points = npy.insert(trans, [trans.shape[1]], moving_points[:, -1].reshape(-1, 1), axis = 1)
+        trans_points = npy.insert(trans_points, [trans_points.shape[1]], moving_points[:, -1].reshape(-1, 1), axis = 1)
         trans_points = npy.append(trans_points, npy.array([[-1, -1, -1, -1]]), axis = 0)
         
-        # Clear the temp files
-        #eg.clear_temp_file()
-        
-        S1 = ml.ones([3, 3], dtype = npy.float32) * para2[3]
-        C = npy.asmatrix(para2[:3]).T
-        
-        C2 = npy.asmatrix(para2[4:7]).T
-        T0 = npy.asmatrix(para[4:]).T
-        R = self.quaternion2rotation(para[:4])
-        T = S1 * T0 + C2 - C
+        matrix = icp.GetMatrix()
+        T = ml.mat([matrix.GetElement(0, 3), matrix.GetElement(1, 3), matrix.GetElement(2, 3)]).T;
+        R = ml.mat([[matrix.GetElement(0, 0), matrix.GetElement(0, 1), matrix.GetElement(0, 2)], 
+                    [matrix.GetElement(1, 0), matrix.GetElement(1, 1), matrix.GetElement(1, 2)], 
+                    [matrix.GetElement(2, 0), matrix.GetElement(2, 1), matrix.GetElement(2, 2)]]).I;
         if (fixed_bif >= 0) and (moving_bif >= 0):
             T[2] += (fixed_bif * fixed_res[2] - moving_bif * moving_res[2])
             
         moving_center = movingData.getPointSet('Centerline').copy();
         result_center = moving_center[npy.where(moving_center[:, 0] >= 0)]
         result_center[:, :3] *= moving_res[:3]
-        result_center[:, :3] -= C.T
-        temp = ml.mat(result_center[:, :3]) * R + ml.ones((result_center.shape[0], 1)) * (T + C).T
+        temp = ml.mat(result_center[:, :3]) * R + ml.ones((result_center.shape[0], 1)) * T.T
         
         result_center[:, :3] = temp
         result_center[:, :3] /= fixed_res[:3]
@@ -86,29 +112,9 @@ class GmmregPointsetRegistration(RegistrationBase):
         
         transform = sitk.Transform(3, sitk.sitkAffine)
         transform.SetParameters(R.reshape(1, -1).tolist()[0] + T.T.tolist()[0])
-        transform.SetFixedParameters(C.T.tolist()[0])
         
         movingImage = movingData.getSimpleITKImage()
         fixedImage = fixedData.getSimpleITKImage()
         resultImage = sitk.Resample(movingImage, fixedImage, transform, sitk.sitkLinear, 0, sitk.sitkFloat32)
         
         return sitk.GetArrayFromImage(resultImage), {'Contour': trans_points, 'Centerline': result_center}
-    def quaternion2rotation(self, q):
-        R = ml.zeros([3, 3], dtype = npy.float32)
-        x, y, z, r = q
-        x2, y2, z2, r2 = q * q
-        
-        R[0, 0] = r2 + x2 - y2 - z2
-        R[1, 1] = r2 - x2 + y2 - z2
-        R[2, 2] = r2 - x2 - y2 + z2
-        
-        R[1, 0] = 2 * (x * y + r * z)
-        R[2, 0] = 2 * (z * x - r * y)
-        R[2, 1] = 2 * (y * z + r * x)
-        R[0, 1] = 2 * (x * y - r * z)
-        R[0, 2] = 2 * (z * x + r * y)
-        R[1, 2] = 2 * (y * z - r * x)
-        
-        ss = x2 + y2 + z2 + r2
-        R = R / ss
-        return R
