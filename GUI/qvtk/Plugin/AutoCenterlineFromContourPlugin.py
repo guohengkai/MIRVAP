19fb9a64a6f6587ca7ce92f80ac68af2e0d82530
guohengkai <guohengkaighk@gmail.com>
2014-04-04 21:55:07 +0800

    Add auto centerline from contour

--- GUI/qvtk/Plugin/AutoCenterlineFromContourPlugin.py
+++ GUI/qvtk/Plugin/AutoCenterlineFromContourPlugin.py
@@ -15,22 +15,27 @@ class AutoCenterlineFromContourPlugin(ContourPlugin):
         ch = self.parent.window_interactor.GetKeySym()
         if ch == 'Return':
             # Get the input information (begin, end, bifurcation)
-            temp = self.parent.parent.getData().getPointSet('Contour')
-            point_vital = [0] * 3
-            for i in range(3):
-                point_vital[i] = temp[npy.round(temp[:, -1]) == i].copy()
-                if point_vital[i].shape[0] == 0:
-                    return
-            
-            bottom = int(npy.round(npy.min(point_vital[0][:, 2])))
-            bif = int(npy.round(npy.max(point_vital[0][:, 2])))
-            up1 = int(npy.round(npy.max(point_vital[1][:, 2])))
-            up2 = int(npy.round(npy.max(point_vital[2][:, 2])))
-            print bottom, bif
-            print up1, up2
-            
-            
-            
+            point_array_result = self.parent.parent.getData().pointSet
+            point_data_result = npy.array(point_array_result.getData('Contour'))
+            center_data = npy.array([[-1, -1, -1, -1]])
+            if point_data_result is None or not point_data_result.shape[0]:
+                return
+            zmin = int(npy.min(point_data_result[:, 2]) + 0.5)
+            zmax = int(npy.max(point_data_result[:, 2]) + 0.5)
+            for cnt in range(3):
+                point_result = point_data_result[npy.where(npy.round(point_data_result[:, -1]) == cnt)]
+                if not point_result.shape[0]:
+                    continue
+                    
+                for i in range(zmin, zmax + 1):
+                    data = point_result[npy.where(npy.round(point_result[:, 2]) == i)]
+                    if data is not None:
+                        if data.shape[0] == 0:
+                            continue
+                        center_data = npy.append(center_data, npy.mean(data, axis = 0).reshape(1, -1), axis = 0)
+                        
+            point_array_result.data['Centerline'] = center_data
+            self.parent.updateAfter()
             self.parent.render_window.Render()
             return
         super(AutoCenterlineFromContourPlugin, self).KeyPressCallback(obj, event)
