3cfd5baae2cd4c382a6a1c2aecc30f371b6c1db8
guohengkai <guohengkaighk@gmail.com>
2014-04-07 20:23:51 +0800

    Adjust the intensity when saving img

--- Script/Save/SaveSliceImg.py
+++ Script/Save/SaveSliceImg.py
@@ -7,6 +7,7 @@ Created on 2014-04-07
 
 from MIRVAP.Script.SaveBase import SaveBase
 from MIRVAP.GUI.qvtk.WidgetView.ResultImageView import ResultImageView
+import numpy as npy
 import cv2
 
 class SaveSliceImg(SaveBase):
@@ -23,15 +24,16 @@ class SaveSliceImg(SaveBase):
                 status = window.widgetView.getDirectionAndSlice()
                 slice = int(status[1] - 0.5)
                 if window.widgetView.view == 2:
-                    img = imgData[slice, :, :]
+                    img = imgData[slice, :, :].copy()
                 elif window.widgetView.view == 1:
-                    img = imgData[:, slice, :]
+                    img = imgData[:, slice, :].copy()
                 elif window.widgetView.view == 0:
-                    img = imgData[:, :, slice]
+                    img = imgData[:, :, slice].copy()
                 else:
                     return False
                 
-                cv2.imwrite(dir, img)
+                img = (img - npy.min(img)) / (npy.max(img) - npy.min(img)) * 255
+                cv2.imwrite(dir, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
                 return True
                 
             return False
