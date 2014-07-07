79b941a944b802952ad51d3bc63d8c7e132ce4bb
guohengkai <guohengkaighk@gmail.com>
2014-07-08 00:23:29 +0800

    Finish configuration py2exe

--- convert2exe.py
+++ convert2exe.py
@@ -10,12 +10,13 @@ import py2exe
 py2exe_options = dict(excludes = ['_ssl',  # Exclude _ssl
                                 'pyreadline', 'doctest', 'locale', 
                                 'optparse', 'calendar', 'pdb', 
-                                'inspect', "pywin", "pywin.debugger", 
+                                "pywin", "pywin.debugger", 
                                 "pywin.debugger.dbgcon", "pywin.dialogs", "pywin.dialogs.list", 
                                 "Tkconstants", "Tkinter", "tcl"],  # Exclude standard library
                       includes = ["sip", "PyQt4.QtOpenGL", "vtk.vtkRenderingPythonSIP", 
                                 "vtk.vtkCommonPythonSIP", "vtk.vtkFilteringPythonSIP", 
-                                "scipy.sparse.csgraph._validation"], 
+                                "scipy.sparse.csgraph._validation", "scipy.special._ufuncs_cxx", 
+								"skimage._shared.geometry"], 
                       compressed = True,  # Compress library.zip
                       )
 setup(windows = ["main.py"], options = {'py2exe': py2exe_options})
